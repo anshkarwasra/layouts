4b21ae7e803ffdaf7c53ff2c09edf489250644f2
anshkarwasra <anshkarwasra15@gmail.com>
2024-12-01 02:47:17 +0530

    updates

---
+++ app/(blogs)/layout.js
@@ -0,0 +1,18 @@
+
+
+
+export const metadata = {
+  title: "myNextApp blogs",
+  description: "desc of my app's blogs",
+};
+
+export default function BlogsLayout({ children }) {
+  return (
+    
+       <>
+        <span>admin navbar</span>
+        {children}
+       </>
+     
+  );
+}
